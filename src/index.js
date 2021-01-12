72885b39d5107c9bc4478b4e96313d393421da10
bk973 <glukooya@gmail.com>
2021-01-12 13:21:28 +0300

    ADDITION: Added body-parser to express middleware

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 /**App config... */
 const express = require('express');
+const bodyParser = require('body-parser')
 const router = require('./router');
 const usersRouter = require('./router/users');
-const app = express();
 
 //middleware...
+
+const app = express();
+
 app.use('/',router);
 app.use('/users/', usersRouter);
 
